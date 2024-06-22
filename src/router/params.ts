fcffb44cc84ff969997bb6ef9a688eeb92bf87ea
Evan Sutherland <stackoverfloweth@gmail.com>
2024-06-22 16:18:20 -0500

    updated to latest version, installed vue devtools, fixed default param

--- src/router/params.ts
+++ src/router/params.ts
@@ -1,4 +1,4 @@
-import { ParamGetSet } from "@kitbag/router"
+import { createParam } from "@kitbag/router"
 
 type SortDirection = 'asc' | 'desc'
 
@@ -6,27 +6,10 @@ function isSortDirection(value: unknown): value is SortDirection {
   return !!value && typeof value === 'string' && ['asc', 'desc'].includes(value)
 }
 
-export const sortParam:ParamGetSet<SortDirection> = {
-  get: (value, {invalid}) => {
-    if(value === undefined){
-      return 'asc'
-    }
-    
-    if(isSortDirection(value)){
-      return value as SortDirection
-    }
-    
-    throw invalid(`value provided does not satisfy `)
-  },
-  set: (value, {invalid}) => {
-    if(value === undefined){
-      return 'asc'
-    }
-  
+export const sortParam = createParam((value, {invalid}) => {
     if(isSortDirection(value)){
       return value
     }
-  
+    
     throw invalid(`value provided does not satisfy `)
-  }
-}
+  }, 'asc')
