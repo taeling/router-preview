915d63a49535e00975604dd2ab04fbe1061753f6
Evan Sutherland <stackoverfloweth@gmail.com>
2024-07-25 13:53:29 -0500

    updated for v0.6+

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import './style.css';
-import { router } from './router';
 import App from './App.vue';
+import { router } from './router';
 
 const app = createApp(App);
 
@@ -11,6 +11,6 @@ declare module '@kitbag/router' {
   }
 }
 
-app.use(router);
+app.use(router)
 
 app.mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,33 @@
-import { createRoutes, createRouter, query } from '@kitbag/router';
-import HomeView from '../views/HomeView.vue';
-import { sortParam } from './params';
+import { createRouter, createRoute, query } from "@kitbag/router";
+import HomeView from "../views/HomeView.vue";
+import { sortParam } from "./params";
 
-const routes = createRoutes([
-  {
-    name: 'home',
-    path: '/',
-    component: HomeView,
-  },
-  {
-    name: 'settings',
-    path: '/settings',
-    query: 'search=[?search]',
-    component: () => import('../views/SettingsView.vue'),
-    children: createRoutes([
-      {
-        name: 'profile',
-        path: '/profile',
-        component: () => import('../views/SettingsProfileView.vue'),
-      },
-      {
-        name: 'keys',
-        path: '/keys',
-        query: query('sort=[?sort]', { sort: sortParam }),
-        component: () => import('../views/SettingsKeysView.vue'),
-      },
-    ])
-  },
-]);
+const home = createRoute({ 
+  name: 'home', 
+  path: '/', 
+  component: HomeView 
+})
 
-export const router = createRouter(routes);
+const settings = createRoute({
+  name: 'settings',
+  path: '/settings',
+  query: 'search=[?search]',
+  component: () => import('../views/SettingsView.vue')
+})
+
+const profile = createRoute({
+  parent: settings,
+  name: 'profile',
+  path: '/profile',
+  component: () => import('../views/SettingsProfileView.vue')
+})
+
+const keys = createRoute({
+  parent: settings,
+  name: 'keys',
+  path: '/keys',
+  query: query('sort=[?sort]', { sort: sortParam }),
+  component: () => import('../views/SettingsKeysView.vue')
+})
+
+export const router = createRouter([home, settings, profile, keys])
