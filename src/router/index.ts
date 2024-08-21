a5589ad8b32e5e529323bb869fdc87aef1863f1c
Evan Sutherland <stackoverfloweth@gmail.com>
2024-08-21 15:19:18 -0500

    using latest, wrap imports in defineAsyncComponent

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createRoute, query } from "@kitbag/router";
 import HomeView from "../views/HomeView.vue";
 import { sortParam } from "./params";
+import { defineAsyncComponent } from "vue";
 
 const home = createRoute({ 
   name: 'home', 
@@ -12,14 +13,14 @@ const settings = createRoute({
   name: 'settings',
   path: '/settings',
   query: 'search=[?search]',
-  component: () => import('../views/SettingsView.vue')
+  component: defineAsyncComponent(() => import('../views/SettingsView.vue'))
 })
 
 const profile = createRoute({
   parent: settings,
   name: 'profile',
   path: '/profile',
-  component: () => import('../views/SettingsProfileView.vue')
+  component: defineAsyncComponent(() => import('../views/SettingsProfileView.vue'))
 })
 
 const keys = createRoute({
@@ -27,7 +28,9 @@ const keys = createRoute({
   name: 'keys',
   path: '/keys',
   query: query('sort=[?sort]', { sort: sortParam }),
-  component: () => import('../views/SettingsKeysView.vue')
+  component: defineAsyncComponent(() => import('../views/SettingsKeysView.vue'))
 })
 
-export const router = createRouter([home, settings, profile, keys])
+export const router = createRouter([home, settings, profile, keys], {
+  base: '/p'
+})
