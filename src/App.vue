8066b462f41386d5828a49b762c255f85b1e57f0
taeling <thomas@taeling.com>
2025-01-27 10:46:03 +0000

    Custom app link

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
+import AppLink from './components/AppLink.vue';
 import NavigationBar from './components/NavigationBar.vue';
 </script>
 
 <template>
   <NavigationBar>
+    <AppLink :to="(resolve) => resolve('home')">Home</AppLink>
     <router-link :to="(resolve) => resolve('home')">Home</router-link>
     <router-link :to="(resolve) => resolve('settings')">Settings</router-link>
   </NavigationBar>
