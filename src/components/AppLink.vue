4b0b77e43333ac28464e9f70f9ef5a9cd3a9e731
taeling <thomas@taeling.com>
2025-01-27 10:56:29 +0000

    App link

---
+++ src/components/AppLink.vue
@@ -0,0 +1,14 @@
+<template>
+    <router-link v-bind="props" style="color:red">
+        <slot></slot>
+    </router-link>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from '@kitbag/router';
+
+type Props = InstanceType<typeof RouterLink>['$props'];
+
+const props = defineProps<Props>();
+
+</script>
